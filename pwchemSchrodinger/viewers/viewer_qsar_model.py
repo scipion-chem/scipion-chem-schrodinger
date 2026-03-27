71e2f0a023493e7fb331b05b6e3c2b3592e9e4bb
blanca <blanca.pueche@cnb.csic.es>
2026-03-27 10:11:13 +0100

    pharm qsar prot output

--- pwchemSchrodinger/viewers/viewer_qsar_model.py
+++ pwchemSchrodinger/viewers/viewer_qsar_model.py
@@ -37,7 +37,7 @@ from ..viewers.viewers_data import MaestroView
 
 class ProtSchrodingerQSARViewer(pwviewer.ProtocolViewer):
     _label = 'Viewer Schrodinger QSAR'
-    _targets = [ProtSchrodingerQSAR]
+    _targets = [ProtSchrodingerQSAR, ProtSchrodingerQSARPharmacophore]
 
     def __init__(self, **args):
         super().__init__(**args)
